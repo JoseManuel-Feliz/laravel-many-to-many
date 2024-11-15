7b0cff200b68ed8201650704d52e2ec8ea71f7be
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-11-15 09:12:42 +0100

    refactor: refactored projects table

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,57 +1,111 @@
 @extends('layouts.app')
 
-@section('page-title', 'Projects')
-
-@section('nav-title','Projects')
-
 @section('main-content')
+
 <div class="container">
 
-    <div><a class="btn btn-success" href="{{route('admin.projects.create')}}">create</a></div>
+    <div class="row">
 
+        <!-- Create New Project btn -->
+        <div class="col mb-3">
+            <a class="btn btn-success" href="{{route('admin.projects.create')}}">
+                Create
+            </a>
+        </div>
 
-    <table class="table table-striped table-bordered table-hover">
-        <thead>
-            <tr>
-                <th>Project title</th>
-                <th>Project date</th>
-                <th>Project status</th>
-                <th>Link repository</th>
-                <th>Image url</th>
-                <th>Description</th>
-                <th colspan="3">Btns</th>
-            </tr>
-        </thead>
-        <tbody class="table-group-divider">
-            <tr>
+        <!-- Projects table -->
+        <div class="col-12 table-responsive">
+            <table class="table table-striped table-bordered table-hover align-middle">
 
-                @forelse($projects as $project)
+                <!-- Table header -->
+                <thead class="text-center">
+                    <tr>
+                        <th>Project title</th>
+                        <th>Project date</th>
+                        <th>Project status</th>
+                        <th colspan="2">Technologies</th>
+                        <th colspan="2">Link repository</th>
+                        <th>Image url</th>
+                        <th>Description</th>
+                        <th colspan="2">Btns</th>
+                    </tr>
+                </thead>
 
+                <!-- Table body -->
+                <tbody class="table-group-divider">
+                    <tr>
 
-                <td>{{$project->project_title}}</td>
-                <td>{{$project->launch_date}}</td>
-                <td>{{$project->project_status}}</td>
-                <td>{{$project->repository_url}}</td>
-                <td>{{$project->project_thumbnail}}</td>
-                <td>{{Str::words($project->project_summary,5,'...')}}</td>
-                <td><a class="btn btn-secondary" href="{{route('admin.projects.show',$project->id)}}">show</a></td>
-                <td><a class="btn btn-warning" href="{{route('admin.projects.edit',$project->id)}}">edit</a></td>
-                <td>
-                    <form action="{{route('admin.projects.delete',$project->id)}}" method="POST">
-                        @csrf
-                        {{method_field('DELETE')}}
-                        <button class="btn btn-danger" type="submit">delete</button>
-                    </form>
-                </td>
-            </tr>
+                        @forelse($projects as $project)
 
-            @empty
-            <tr>
-                <td colspan="7">There aren't projects</td>
-            </tr>
-            @endforelse
-        </tbody>
-    </table>
+                        <!-- Project Title -->
+                        <td>{{$project->project_title}}</td>
 
+                        <!-- Project launch date -->
+                        <td>{{$project->launch_date}}</td>
+
+                        <!-- Project status -->
+                        <td>{{$project->status->project_status}}</td>
+
+                        <!-- Project technologies -->
+                        <td colspan="2" class="text-center">
+                            @forelse($project->technologies as $tech)
+
+                            <span class="badge text-bg-success rounded-pill">
+                                <!-- Single techonology -->
+                                {{$tech->name}}
+                            </span>
+
+                            @empty
+                            <!-- message when there are no technology -->
+                            There aren't technologies
+
+                            @endforelse
+
+                        </td>
+
+                        <!-- Project repository link -->
+                        <td colspan="2">{{$project->repository_url}}</td>
+
+                        <!-- Project img -->
+                        <td>{{$project->project_thumbnail}}</td>
+
+                        <!-- Project summary -->
+                        <td>{{Str::words($project->project_summary,5,'...')}}</td>
+
+                        <!-- Actions btns -->
+                        <td colspan="2">
+
+                            <!-- Show btn -->
+                            <a class="btn btn-secondary me-1"
+                                href="{{route('admin.projects.show',$project->id)}}">show</a>
+
+                            <!-- Edit btn -->
+                            <a class="btn btn-warning me-1"
+                                href="{{route('admin.projects.edit',$project->id)}}">edit</a>
+
+                            <!-- Delete form -->
+                            <form class="d-inline" action="{{route('admin.projects.destroy',$project->id)}}"
+                                method="POST">
+                                @csrf
+                                {{method_field('DELETE')}}
+
+                                <!-- Delete btn -->
+                                <button class="btn btn-danger" type="submit">delete</button>
+                            </form>
+                        </td>
+                    </tr>
+
+                    @empty
+
+                    <!-- message when there are no projects -->
+                    <tr>
+                        <td colspan="7">There aren't projects</td>
+                    </tr>
+                    @endforelse
+                </tbody>
+            </table>
+
+        </div>
+    </div>
 </div>
 @endsection

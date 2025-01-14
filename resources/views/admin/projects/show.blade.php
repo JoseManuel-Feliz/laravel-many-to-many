afc4ebc2bde5d81cfae802a4a299042eef7870e8
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2025-01-14 23:15:53 +0100

    feat: project update

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,14 +1,22 @@
 @extends('layouts.app')
 
 @section('main-content')
+
 <div class="container">
-
-    <p>{{$project->project_title}}</p>
-    <p>{{$project->launch_date}}</p>
-    <p>{{$project->project_status}}</p>
-    <p>{{$project->repository_url}}</p>
-    <p>{{$project->project_thumbnail}}</p>
-    <p>{{$project->project_summary}}</p>
-
+    <div class="row justify-content-center">
+        <div class="col-4">
+            <div class="card">
+                <img class="card-img-to" src="{{$project->project_thumbnail}}" alt="{{$project->project_title}}">
+                <div class="card-body">
+                    <h3 class="card-title">{{$project->project_title}}</h3>
+                    <p class="card-text">{{$project->project_summary}}</p>
+                    <p>{{$project->tecnolologies}}</p>
+                    <a class="card-link" href="{{$project->repository_url}}">See my project</a>
+                    <p>{{$project->project_status}}</p>
+                    <p>{{$project->launch_date}}</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
